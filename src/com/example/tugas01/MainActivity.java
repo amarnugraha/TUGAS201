f891150744e5de6ddae71be4a76ce26bc95aba80
amarnugraha <yusufamar57@gmail.com>
2019-11-20 18:04:43 +0700

    tambah contact

--- src/com/example/tugas01/MainActivity.java
+++ src/com/example/tugas01/MainActivity.java
@@ -15,6 +15,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -26,6 +27,7 @@ public class MainActivity extends Activity {
 	private String TAG = MainActivity.class.getSimpleName();
     private ProgressDialog pDialog;
     private ListView lv;
+    private Button btambah;
  
     // URL to get contacts JSON
     private static String url = "http://apilearning.totopeto.com/contacts";
@@ -40,6 +42,7 @@ public class MainActivity extends Activity {
         contactList = new ArrayList<HashMap<String, String>>();
         
         lv = (ListView) findViewById(R.id.list);
+        btambah = (Button) findViewById(R.id.bttambah);
  
         new GetContacts().execute();
         
@@ -51,6 +54,16 @@ public class MainActivity extends Activity {
 				HashMap<String, String> hm = contactList.get(position);
 				Intent intent = new Intent(MainActivity.this, ContactDetail.class);
 				intent.putExtra("id", hm.get("id"));
+				startActivity(intent);
+			}
+		});
+        
+        btambah.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(MainActivity.this, TambahContact.class);
 				startActivity(intent);
 			}
 		});
